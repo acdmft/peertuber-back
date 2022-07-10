32c2825849ffb823548b859676bc025655b12e68
fusbudget <andrewsanser@gmail.com>
2022-07-10 20:46:34 +0200

    add /login, /signup, /logout routes in server.js

--- server.js
+++ server.js
@@ -3,4 +3,32 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+//mongoDB
+const MONGO_URI = process.env.MONGO_URI;
+const mongoose = require("mongoose");
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+}).then(()=> {
+  console.log("Connected to MongoDB");
+});
+
+// MIDDLEWARES
+app.use(express.json());
+
+// ROUTERS 
+const loginRouter = require("./routers/loginRouter");
+const signupRouter = require("./routers/signupRouter");
+
+// ROUTES 
+app.get("/", async (_req, res) =>{
+  res.status(200).json({message: "Home page"})
+});
+app.use("/login", loginRouter); // LOGIN (POST)
+app.use("/signup", signupRouter); // NEW ACCOUNT (POST)
+// LOGOUT 
+app.get("/logout", (_req, res) => {
+  res.clearCookie("jwt");
+  res.json("You are logged out");
+}); 
+
 app.listen(port, console.log(`Connected to the port ${port}`));
