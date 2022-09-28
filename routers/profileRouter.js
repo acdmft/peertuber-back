c780d3dd1c1b14d63f33cd4a8aec201a7522fbaf
fusbudget <andrewsanser@gmail.com>
2022-09-28 20:48:57 +0200

    add profileRouter

---
+++ routers/profileRouter.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const isLoggedIn = require("../middlewares/isLogged");
+const router = express.Router();
+// USER MODEL
+const User = require("../models/User"); 
+
+router.get("/", isLoggedIn, async (req, res) => {
+  let userId = req.data.userId;
+  let user;
+  try {
+    const projection = { _id: 0, password: 0, isAdmin: 0, isModerator: 0 }
+    user = await User.findOne({ _id: userId }, projection )
+  } catch (err) {
+    return res.status(400).json({message: err});
+  }
+  return res.status(200).json(user);
+});
+
+
+module.exports = router;
