dc66a65b51df7872e20f99c736c187e701f543dc
acdmft <crankyfox@protonmail>
2022-11-21 20:32:19 +0100

    add delete method in profilerouter

--- routers/profileRouter.js
+++ routers/profileRouter.js
@@ -16,5 +16,16 @@ router.get("/", isLoggedIn, async (req, res) => {
   return res.status(200).json(user);
 });
 
+router.delete("/", isLoggedIn, async (req, res) => {
+  let userId = req.data.userId;
+  let result;
+  try {
+    result = await User.deleteOne({ _id: userId });
+  } catch (err) {
+    return res.status(400).json( { error: err });
+  }
+  res.clearCookie("jwt");
+  return res.status(200).json(result);
+});
 
 module.exports = router;
